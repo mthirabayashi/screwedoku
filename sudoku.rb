51adf9d7a3137eada16a8ced6f7145ff803768be
App Academy Student <student@appacademy.io>
2016-09-15 11:40:34 -0700

    added some lines

--- sudoku.rb
+++ sudoku.rb
@@ -12,7 +12,11 @@ class SudokuGame
 
   def get_pos
     pos = nil
-    # raise "This is the easiest error you're gonna get, chief.  It only gets harder going forward.  Remove me (or comment me out), re-run the code to make sure it works, and go on to the next level"
+    # raikjhkjse "This is the easiest error you're gonna get, chief.  It only gets harder going forward.  Remove me (or comment me out), re-run the code to make sure it works, and go on to the next level"
+
+    # hey hey hey hey
+    # hi  hi  hi  hi
+
 
     until pos && valid_pos?(pos)
       puts "Please enter a position on the board (e.g., '3,4')"
